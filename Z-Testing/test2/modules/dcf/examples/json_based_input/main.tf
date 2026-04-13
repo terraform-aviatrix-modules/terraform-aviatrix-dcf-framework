5422dc8a1a365623e1b890c6f5446858fc8f83f0
Matthew Gillespie <mgillespie+GIT@aviatrix.com>
2026-04-13 11:56:44 -0400

    FMT changes

---
+++ Z-Testing/test2/modules/dcf/examples/json_based_input/main.tf
@@ -0,0 +1,16 @@
+# Create an Aviatrix Distributed Firewalling config
+resource "aviatrix_distributed_firewalling_config" "test" {
+  enable_distributed_firewalling = true
+}
+
+module "dcf" {
+  source = "../.."
+
+  smart_groups = try(local.policy.smart_groups, {})
+  web_groups   = try(local.policy.web_groups, {})
+  rulesets     = try(local.policy.rulesets, {})
+}
+
+locals {
+  policy = jsondecode(file("${path.module}/policy.json"))
+}
